1df7676257a7107d70b63a440a96a82a410ab294
HampeBea <Hampe_rock@hotmail.com>
2017-11-08 13:55:13 +0100

    made a window 500x500
    made a window 500x500

---
+++ src/circle/main.java
@@ -0,0 +1,25 @@
+package circle;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		Group root = new Group();
+		Scene scene = new Scene(root, 500, 500);
+
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+}
